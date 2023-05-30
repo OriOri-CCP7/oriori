b5aaf5fd6107dc6ee4cc2da755c7f89d6bf901e0
nikobatzi1990 <nicole.boci@gmail.com>
2023-05-30 19:58:44 +0900

    wip: write endpoints for Product model

--- api/views.py
+++ api/views.py
@@ -1,7 +1,15 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from ..base import <insert model class here>
-from ..serializers import <insert serializer class here>
+from ..base.models import User
+from ..base.models import Favorite
+from ..base.models import Location
+from ..base.models import Store
+from ..base.models import Product
+from ..base.serializers import UserSerializer
+from ..base.serializers import FavoriteSerializer
+from ..base.serializers import LocationSerializer
+from ..base.serializers import StoreSerializer
+from ..base.serializers import ProductSerializer
 
 # Create your views here.
 
@@ -9,46 +17,78 @@ from ..serializers import <insert serializer class here>
 def hello(request):
    return Response('Hello World 🌎')
 
-# Views for user's data
+# Views for User data
 @api_view(['GET'])
 def getUserData(request):
   query_parameter = request.query_params.get('username')
-  user = Users.objects.get(username=query_parameter)
-  serializer = UsersSerializer(user, many=False)
+  user = User.objects.get(username=query_parameter)
+  serializer = UserSerializer(user, many=False)
   return Response(serializer.data)
 
 @api_view(['POST'])
 def addNewUser(request):
-  serializer = UsersSerializer(data=request.data)
+  serializer = UserSerializer(data=request.data)
   if serializer.is_valid():
     serializer.save()
   return Response(serializer.data)
 
 @api_view(['PATCH'])
 def editUserData(request, uuid):
-  user = Users.objects.get(uuid = uuid)
+  user = User.objects.get(uuid = uuid)
   user.username = request.data.get('username', user.username)
   user.email = request.data.get('email', user.email)
   user.location_id = request.data.get('location_id', user.location_id)
   user.save()
   return Response(user)
 
-# Views for favorites data
+# Views for Favorites data
 @api_view(['GET'])
 def getUserFavorites(request):
-  favorites = Favorites.objects.select_related('users_set')
-  serializer = FavoritesSerializer(favorites, many=True)
+  favorites = Favorite.objects.select_related('users_set')
+  serializer = FavoriteSerializer(favorites, many=True)
   return Response(serializer.data)
 
 @api_view(['POST'])
 def addNewFavorite(request):
-  serializer = FavoritesSerializer(data=request.data)
+  serializer = FavoriteSerializer(data=request.data)
   if serializer.is_valid():
     serializer.save()
   return Response(serializer.data)
 
 @api_view(['DELETE'])
 def removeFavorite(request, pk):
-  favorite = Favorites.objects.get(pk=pk)
+  favorite = Favorite.objects.get(pk=pk)
   favorite.delete()
   return Response("Favorite Deleted")
+
+# Views for Product data
+@api_view(['GET'])
+def getProductData(request):
+  query_parameter = request.query_params.get('name')
+  product = Product.objects.get(product_name=query_parameter)
+  serializer = ProductSerializer(product)
+  return Response(serializer)
+
+@api_view(['POST'])
+def addNewProduct(request):
+  serializer = ProductSerializer(data=request.data)
+  if serializer.is_valid():
+    serializer.save()
+  return Response(serializer.data)
+
+@api_view(['PATCH'])
+def editProductData(request, name):
+  product = Product.objects.get(product_name=name)
+  product.product_name = request.data.get('product_name', product.product_name)
+  product.store_id = request.data.get('store_id', product.store_id)
+  product.start_date = request.data.get('start_date', product.start_date)
+  product.end_date = request.data.get('end_date', product.end_date)
+  product.sources = request.data.get('sources', product.sources)
+  product.save()
+  return Response(product)
+
+@api_view(['DELETE'])
+def deleteProductData(request, name):
+  product = Product.objects.get(product_name=name)
+  product.delete()
+  return Response("Product Deleted")
