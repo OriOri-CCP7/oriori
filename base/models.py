3ba271f6ca261b7b6ffe8a3bcdefd9c2a4a8d2bb
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2023-05-30 19:12:55 +0800

    fix: erase unique constraint from Location country field and postgis engine for db
    Co-authored-by: kappanjoe <kappanjoe@users.noreply.github.com>
    Co-authored-by: ikuno815 <ikuno815@users.noreply.github.com>

--- base/models.py
+++ base/models.py
@@ -3,7 +3,7 @@ from django.contrib.gis.db import models
 # Create your models here.
 
 class Location(models.Model):
-    country = models.CharField(max_length=255, unique=True)
+    country = models.CharField(max_length=255)
     prefecture = models.CharField(max_length=255)
 
 class Store(models.Model):
