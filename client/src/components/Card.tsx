0e62549b3a6c0e359ef1604dfb95881930fd7728
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2023-05-31 21:13:24 +0800

    creating TS React Card Component

---
+++ client/src/components/Card.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+type = Props = {
+    className: string, 
+    productName: string,
+    offerStart: {
+        timestamp: number, 
+        year:number, 
+        month:number, 
+        day:number
+    },
+    offerEnd: {
+        timestamp: number, 
+        year:number, 
+        month:number, 
+        day:number
+    }, 
+    favourNumber: number, 
+    onClick: (fn: ({})), 
+    onChange: (fn: {{}})
+}
+
+const Card :React.FC<Props> = (Props) => {
+    return ()
+}
+
