38e53a16ef347859f80dad04419fe69094abc7b3
ikuno815 <ikuno.kanasugi19@gmail.com>
2023-06-03 11:43:07 +0900

    feat: make Home, Favorite, Popular, Search pages and define path in App.tsx with react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Favorite from './pages/Favorites';
+import Popular from './pages/Popular';
+import Search from './pages/Search';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/favorite' element={<Favorite/>}></Route>
+        <Route path='/popular' element={<Popular/>}></Route>
+        <Route path='/search' element={<Search/>}></Route>
+      </Routes>
+      <Navbar/>
+      </BrowserRouter>
     </div>
   );
 }
