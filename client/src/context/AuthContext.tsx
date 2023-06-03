89db628c95dbf09fc05d94ae31f2b72055900177
nikobatzi1990 <nicole.boci@gmail.com>
2023-06-03 12:22:14 +0900

    wip: write functions for user authentication

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,1 +1,46 @@
 export {};
+import { createContext, useContext, useState } from 'react';
+import axios from 'axios';
+import auth from '../firebase.config';
+import { 
+  signInWithEmailAndPassword, 
+  createUserWithEmailAndPassword, 
+  signOut,
+  onAuthStateChanged 
+} from 'firebase/auth';
+
+const UserContext = createContext();
+
+export const AuthContextProvider = ({children}) => {
+  const [user, setUser] = useState({});
+
+  const signup = async (username: string, email: string, password: string) => {
+    let newUserInfo: {username: string, email: string, password: string, uuid: string | unknown}
+    newUserInfo = {
+      username: username,
+      email: email,
+      password: password,
+      uuid: null
+    };
+    const newUser = await createUserWithEmailAndPassword(auth, email, password);
+    newUserInfo.uuid = newUser.user.uid;
+    // AXIOS REQUEST TO PYTHON SERVER HERE
+  };
+
+  const login = async (email: string, password: string) => {
+    const loggedIn = signInWithEmailAndPassword(auth, email, password);
+    return loggedIn;
+  }
+
+  const logout = () => {
+    return signOut(auth);
+  };
+
+  return <UserContext.Provider value={{ signup, login, logout, user }}>
+    {children}
+  </UserContext.Provider>
+}
+
+export const UserAuth = () => {
+  return useContext(UserContext);
+}
--- client/src/firebase.config.tsx
+++ client/src/firebase.config.tsx
@@ -16,4 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-module.exports = auth;
+export default auth;
