147fc835d3b1fa6bec95defe6ec5e255f4a35e16
nikobatzi1990 <nicole.boci@gmail.com>
2023-06-03 16:33:42 +0900

    feat: create basic Signup page component

---
+++ client/src/pages/Signup.tsx
@@ -0,0 +1,34 @@
+export {};
+import React, { useState } from "react";
+import { UserAuth } from "../context/AuthContext";
+import "./Signup.css";
+
+type Props = { 
+  className: string, 
+};
+
+const Signup: React.FC<Props> = () => {
+  const auth = UserAuth();
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    try {
+      if (auth) {
+        auth.signup(username, email, password);
+      } 
+    } catch (error) {
+        console.log("🤬", error);
+      }
+  };
+  
+  return (
+    <>
+
+    </>
+  );
+};
+
+export default Signup;
