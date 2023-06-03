803ec502f952c51f7be46cfcbeb5ff7d904b6be2
ikuno815 <ikuno.kanasugi19@gmail.com>
2023-06-03 12:10:21 +0900

    feat: create navbar component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
-import './Navbar.css'
+import './Navbar.css';
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
+  const navigate = useNavigate();
   return (
-    <div>Navbar</div>
+    <div className='nav'>
+      <ul>
+            <li>
+              <button onClick={() => navigate('/')} className="menu-title">Home</button>
+            </li>
+            <li>
+              <button onClick={() => navigate('/favorite')} className="menu-title">Favorite</button>
+            </li>
+            <li>
+              <button onClick={() => navigate('/popular')} className="menu-title">Popular</button>
+            </li>
+            <li>
+              <button onClick={() => navigate('/search')} className="menu-title">Search</button>
+            </li>
+        </ul>
+    </div>
   )
 }
 
