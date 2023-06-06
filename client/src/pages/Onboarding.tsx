c2a3c653463316ab5826696c64b4ec902106fb27
Joe Van Alstyne <6261485+kappanjoe@users.noreply.github.com>
2023-06-06 15:09:36 +0900

    feat: create skeleton Onboarding page

--- client/src/pages/Onboarding.tsx
+++ client/src/pages/Onboarding.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import './Onboarding.css';
+import prefs from '../prefectures.json';
 
 function Onboarding() {
   return (
-    <div>Onboarding</div>
-  )
+    <>
+      <p className="onboarding-text">
+        四季折々<br/>
+        （しきおりおり）<br/>
+        <br/>
+        <b>oriori</b> comes from the
+        Japanese phrase <i>shikioriori</i>,
+        meaning ‘from season to season’
+      </p>
+      <br/>
+      <label>
+        Home Prefecture:
+        <select>
+          {
+            prefs.map((element) => {
+              return (<option value={element.pk}>{element.name}</option>)
+            })
+          }
+        </select>
+      </label>
+      <p>
+        Select the prefecture you want to use as your Home Prefecture.<br/>
+        It can be where you live, where you plan on visiting the most, or just your favorite prefecture!<br/>
+        <i>You can change this at any time in the settings menu.</i>
+      </p>
+    </>
+  );
 }
 
-export default Onboarding
+export default Onboarding;
