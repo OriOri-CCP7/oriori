28a043e3b7e6a6a31ba035771cdd50d511a9e62c
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2023-06-06 16:53:06 +0800

    add basic props on dropdown menu

--- client/src/components/DropdownMenu.tsx
+++ client/src/components/DropdownMenu.tsx
@@ -1,2 +1,16 @@
 import React from 'react';
 import axios from 'axios';
+
+type Props = {
+    className: string | undefined,
+    onChange: (event: React.MouseEvent<HTMLElement>) => void,
+}
+
+export default function DropdownMenu({className,onChange, options, }):(React.FC<Props>)  {
+    
+    return (
+        <>
+            <div></div>
+        </>
+    )
+}
