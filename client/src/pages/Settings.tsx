4879f9a3bb5d276d20f20e49806b483cd2b99f72
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2023-06-07 20:44:00 +0800

    setting pages (WIP)

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import DropdownMenu from "../components/DropdownMenu";
 import {UserAuth} from "../context/AuthContext";
 import Button from "../components/Button";
 import { useNavigate, Link } from "react-router-dom";
 import Input from "../components/Input";
+import axios from 'axios'
 
 interface Props {
  
@@ -12,32 +13,73 @@ interface Props {
 function Settings({}: Props) {
   const auth  = UserAuth();
   const navigate = useNavigate();
+  // const userDatabase = { username, email, uuid, location}
+  let username, email, uuid, location;
+
+  const userData = {};
+  useEffect(()=>{
+    getUserData();
+  },[]);
+  const getUserData = async () => {
+    try {
+      if (auth){
+        const result = await axios.get(`/user/${auth.user.username}/`);
+        setStoredInputUser(result.data);
+      } else {
+        navigate("/logout");
+      }
+    } catch (error) {
+      console.log("🍀", `We got error: ${error}`);
+      
+    } finally {
+      console.log("getUserData attempt has been launched")
+    }
+
+  }
+  
+    
+  
   
   const handleBack = (event:React.MouseEvent<HTMLButtonElement> ) => {
     event.preventDefault();
     navigate('/');
   }
-  const handleUsernameChange: (event: FormEvent<HTMLFormElement>) = (event) =>{
-
+  // FormEvent<HTMLFormElement>
+  const handleUsernameChange=  (event: React.ChangeEvent<HTMLInputElement>) =>{
+    event.preventDefault();
+    
   }
+  const [storedUserName, setStoredInputUser] = useState({});
+  const [storedlocation, setStoredLocation] = useState(location)
+  
   return (
     <div className="Setting">
       <Button className="backButton" type="button" onClick={handleBack} text="Back"/>
       <h1>Settings</h1>
       <h2>Username</h2>
-      <form>
-        <label >
-          Username
-          <Input className = "username" placeholder = {"username"} type = "text" value = { auth.user } onChange = { handleUserNameChange} />
-        </label>
+      <form >
+        <label>Username :</label>
+        
+          <Input className="username" placeholder={"username cannot be empty"} type="text" value={ "" } onChange={ (event) => {
+            event.preventDefault();
+            //setStoredInputUser(event.target.value)
+            }
+            } />
+        <br />
         <label>
-          Email Address
-          <input type="email" defaultValue="example@email.com"/>
+          Email Address:
         </label>
+        <input type="email" defaultValue="example@email.com"/>
+        <br />
+        <div >
         <label>
-          Home Prefecture
-          <DropdownMenu labelName="" setPrefecture={(element) => console.log(element)} />
+          Home Prefecture:
+          
         </label>
+        <DropdownMenu labelName="" setPrefecture={(element) => console.log(element)} />
+        </div>
+        
+        <Button className="SaveButton" type="submit" text="Update" onClick={() => {}}/>
       </form>
     </div>
   );
