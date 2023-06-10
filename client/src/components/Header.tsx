f6dcb8dc6723f9da425b85e05e98c34f821cebcd
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2023-06-10 16:54:18 +0800

    initiate header & navbar css improvement

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Header.css';
 
 type Props = {
-  className: string,
-  mainText: string,
+  className?: string,
+  mainText?: string,
   secondaryText?: string
 }
 
-const Header: React.FC<Props> = ({ className, mainText, secondaryText }) => {
+const Header: React.FC<Props> = ({ className, mainText, secondaryText }: Props) => {
+
+  // useEffect(()=>{
+  //   handleClassName()
+  // },[]);
+
+  // const [headerClassName, setClassName] = useState<string>(className)
+  // const handleClassName = () => {
+  //   if(!headerClassName){
+  //     setClassName("default-header-className");
+  //   }
+  // }
   return (
     <>
     <header
