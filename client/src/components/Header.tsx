6692779be5da79becfdebd8cbbe1bb072acdbd21
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2023-06-12 20:05:58 +0800

    remove bash file; setup small medium large in home.css & nav.css

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,8 +2,8 @@ import React, {useState, useEffect} from 'react';
 import './Header.css';
 
 type Props = {
-  className?: string,
-  mainText?: string,
+  className: string,
+  mainText: string,
   secondaryText?: string
 }
 
