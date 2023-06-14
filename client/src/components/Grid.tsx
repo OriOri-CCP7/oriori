1461335fd6baf340fdc5d1de5b9072d8c63937e7
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2023-06-15 02:59:25 +0800

    experiment with @mui/material/Unstable_Grid2 and props css

--- client/src/components/Grid.tsx
+++ client/src/components/Grid.tsx
@@ -2,16 +2,49 @@ import React from 'react';
 import { UserFavs } from '../context/FavContext';
 import Grid from '@mui/material/Unstable_Grid2';
 import Card from './Card';
+import {styled, makeStyles } from '@mui/material/styles';
+
+type cssClass = {
+  height: number | string | undefined,
+  width: number | string | undefined,
+  padding: number | string | undefined,
+  margin: number | string | undefined,
+  backgroundColor: string | undefined,
+  color: string | undefined
+}
 
 interface Props {
   productArray: Array<Product>,
+  props: cssClass,
+  theme: cssClass,
 };
 
-function GridComponent({ productArray }: Props) {
+function GridComponent({ productArray, props, theme }: Props) {
   let { favorites, isLoadingFavs } = UserFavs();
   console.log("🥰", favorites);
 
+  const cssMakeStyles = makeStyles((theme: cssClass) => ({
+    myClass: {
+      height: theme.height,
+      width: theme.width,
+      padding: theme.padding,
+      margin: theme.margin,
+      backgroundColor: theme.backgroundColor,
+      color: theme.color,
+    },
+  }));
+
+  // const StyledGridItem = styled(Grid)(({ props })=> ({
+  //   height: props.height,
+  //   width: props.width,
+  //   padding: props.padding,
+  //   margin: props.margin,
+  //   backgroundColor: props.backgroundColor,
+  //   color: props.color,
+  // }));
+
   return (
+    <>
     <div className="Grid">
       <Grid container rowSpacing={1}>
           {!isLoadingFavs && productArray.map((product) => (
@@ -24,6 +57,7 @@ function GridComponent({ productArray }: Props) {
           ))}
       </Grid>
     </div>
+    </>
   );
 }
 
