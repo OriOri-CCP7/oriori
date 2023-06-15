10b126d02501ba590856f395b02f0344e1855dcc
Joe Van Alstyne <6261485+kappanjoe@users.noreply.github.com>
2023-06-15 13:04:11 +0900

    feat: add blank default option to DropdownMenu component

--- client/src/pages/Search.tsx
+++ client/src/pages/Search.tsx
@@ -13,31 +13,36 @@ export default function Search() {
   const [products, setProducts] = useState<Product[]>([]);
 
   const getProducts = (prefId: string) => {
-    axios.get(`/api/locations/${prefId}/products/`, {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'X-CSRFToken': auth?.csrftoken ?? ""
-      }
-    })
-    .then((response) => {
+    if (prefId !== "") {
+      axios.get(`/api/locations/${prefId}/products/`, {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'X-CSRFToken': auth?.csrftoken ?? ""
+        }
+      })
+      .then((response) => {
         setProducts(response.data);
-    })
-    .catch((err) => console.log(err));
+      })
+      .catch((err) => console.log(err));
+    } else {
+      setProducts([]);
+    }
   };
 
   return (
     <div>
       <Header
         className="search-header"
-        mainText="Search" />
+        mainText="Search"/>
       <DropdownMenu 
-      labelName='Select a prefecture:' setPrefecture={getProducts} prefill={undefined}/>
-      <ProductGrid productArray={products} />
+        labelName='Select a prefecture:'
+        setPrefecture={ getProducts }/>
+      <ProductGrid productArray={ products }/>
       <Navbar/>
       <Footer 
-        className = "footer"
-        text="© 2023 OriOri" />
+        className="footer"
+        text="© 2023 OriOri"/>
     </div>
   );
 };
