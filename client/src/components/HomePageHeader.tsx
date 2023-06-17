4ddb4c757661c5dc1174c7a7fd8d80c1bec86f5f
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2023-06-17 15:31:51 +0800

    feat: HomePageHeader

---
+++ client/src/components/HomePageHeader.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { UserAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { Cog6ToothIcon } from '@heroicons/react/24/solid';
+import Button from '../components/Button';
+import './HomePageHeader.css';
+import prefs from '../data/prefectures.json';
+
+const HomePageHeader = () => {
+    const auth = UserAuth();
+    const navigate = useNavigate();
+
+    const [location, setLocation ] =useState<number>(Number(auth?.user.location));
+    const [prefecture, setPrefecture] = useState<string>(prefs[location - 1].name);
+    const [username, setUsername] =useState<string>(auth?.user.username ?? '');
+    
+    const handleLogout = async () => {
+        try {
+          if(auth){
+            await auth.logout();
+          }
+          console.log("User Logged Out");
+          navigate('/');
+        } catch (error) {
+          console.log('🥸', error);
+        }
+      };
+
+    return (
+    <>
+    <div className="logout-section">
+      <Button
+        className="logout"
+        text="Log Out"
+        type="button"
+        onClick={ handleLogout } />
+    </div>
+    <div className="setting-section">
+      <div className="circular-icon" style={{ background: '#ccc'}}>
+        <Cog6ToothIcon 
+          className="settings-icon"
+          onClick = {() => {
+            navigate('/settings');
+            }} />
+      </div>
+    </div>
+    <div className="user-section">
+      <div className="user-picture">
+        <div className="circular-icon" style={{ background: 'DodgerBlue', width: '90px', height:'90px' }}>
+          {username}
+          {/* profile picture */}
+          </div>
+          </div>
+          <div>
+          <div className="user-location">
+           {prefecture ? <div>{prefecture}</div> : null}
+
+          </div>
+          
+          <div className="local-production">{"Local Production"}
+          </div>
+        </div>
+      </div>
+    </>
+)
+}
+export default HomePageHeader;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,38 +1,21 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { UserAuth } from "../context/AuthContext";
-import { useNavigate } from "react-router-dom";
-import { Cog6ToothIcon } from '@heroicons/react/24/solid';
+
 import axios from "axios";
 import './Home.css';
-import Button from '../components/Button';
+
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import ProductGrid from "../components/ProductGrid";
 import Navbar from "../components/Navbar";
 
-import prefs from '../data/prefectures.json';
-
-type Prefect = {
-  pk: number,
-  name: string,
-}
-
+import HomePageHeader from "../components/HomePageHeader"
 
 export default function Home() {
-  const navigate = useNavigate();
+  
   const auth = UserAuth();
   const [products, setProducts] = useState<Product[]>([]);
-
-  let prefecture = "";
-  const location = useRef<string>(auth?.user.location ?? '');
-  // const prefecture = useRef<string>(prefs.pk[location.current])
-  const prefObj:Prefect | undefined = prefs.find(pref => pref.pk.toString() === location.current);
-  if(prefObj){
-    prefecture= prefObj.name
-  }
   
-  
-
   useEffect(() => {
     let headers = {
       'Accept': 'application/json',
@@ -51,39 +34,13 @@ export default function Home() {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const handleLogout = async () => {
-    try {
-      if(auth){
-        await auth.logout();
-      }
-      console.log("User Logged Out");
-      navigate('/');
-    } catch (error) {
-      console.log('🥸', error);
-    }
-  };
-
   return (
   <>
-    <div className="buttons">
-      <Button
-      className="logout"
-      text="Log Out"
-      type="button"
-      onClick={ handleLogout } />
-      
-      <Cog6ToothIcon 
-        className="settings-icon"
-        onClick = {() => {
-          navigate('/settings');
-        }} />
-    </div>
-    <div className="user-location">
-      {prefecture}
-    </div>
+    <HomePageHeader />
+    
     <Header
         className="homepage-header"
-        mainText="OriOri Homepage" />
+        mainText="" />
       {
         products.length > 0
         ? <ProductGrid productArray={ products }/>
