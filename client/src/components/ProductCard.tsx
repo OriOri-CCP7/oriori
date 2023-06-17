ec0e24eefb96803b7662a957a6cfc46a9c389f5e
Joe Van Alstyne <6261485+kappanjoe@users.noreply.github.com>
2023-06-17 12:42:01 +0900

    feat: adjust ProductCard component layout

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -23,12 +23,12 @@ function ProductCard ({ product, bookmark, log }: Props) {
   const [isLogged, setIsLogged] = useState(log ? true : false);
   const [isLiked, setIsLiked] = useState(log?.liked_it ? true : false);
 
-  let availabilityMsg = "No availability info.";
+  let availabilityMsg = 'No availability info.';
   
   const currentDate: Date = new Date();
   const currentDateNum = currentDate.getTime();
   const oneDay: number = 24 * 60 * 60 * 1000;
-  let cardClass = "product__card ";
+  let cardClass = 'product__card ';
 
   if (product.start_date) {
     const offerStartDate: Date = new Date(product.start_date);
@@ -39,9 +39,9 @@ function ProductCard ({ product, bookmark, log }: Props) {
   
     
     if (daysSinceStart >= 0) {
-      availabilityMsg = "Now available!";
+      availabilityMsg = 'Now available!';
       if (daysSinceStart < 4) {
-        cardClass += "new "
+        cardClass += 'new '
       }
     }
   }
@@ -53,20 +53,20 @@ function ProductCard ({ product, bookmark, log }: Props) {
 
     if (daysBeforeEnd >= 0) {
       if (daysBeforeEnd < 6) {
-        cardClass += "ending";
+        cardClass += 'ending';
         availabilityMsg = `Only available for ${daysBeforeEnd} days!`;
       } else {
         availabilityMsg = `Available until ${offerEndDate.toLocaleDateString()}`;
       }
     } else {
-      availabilityMsg = "No longer available.";
+      availabilityMsg = 'No longer available.';
     }
   }
 
   const headers = {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
-    'X-CSRFToken': auth?.csrftoken ?? ""
+    'X-CSRFToken': auth?.csrftoken ?? ''
   };
 
   const addBkmarkHandler = () => {
@@ -138,21 +138,28 @@ function ProductCard ({ product, bookmark, log }: Props) {
   };
 
   return (
-      <div className={ cardClass }>
-          <div className="product__img">
-              { product.img_url ? <img src={ product.img_url } alt={ product.product_name }/> : <></> }
-          </div>
-          <div className="product__name">
-              { product.product_name }
-          </div>
-          <div className="product__avail-msg">
-              { availabilityMsg }
-          </div>
-          { isLogged && (isLiked ? 'Tried it, loved it!' : 'Tried it!') }
+    <div className={ cardClass }>
+      <div className='product__title'>
+        <h2 className='product__name'>
+          { product.product_name }
+        </h2>
+        <div className='product__avail-container'>
+          <p className='product__avail-text'>
+            { availabilityMsg }
+          </p>
+        </div>
+      </div>
+      <div className='product__content'>
+        <div className='product__img'>
+          { product.img_url ? <img src={ product.img_url } alt={ product.product_name }/> : <></> }
+        </div>
+        <div className='product__button-container'>
+          <BkmarkButton isBookmark={ isBookmark } clickHandler={ clickBkmarkHandler }/>
           <LogButton isLogged={ isLogged } clickHandler={ clickLogHandler }/>
           { isLogged && <LikeButton isLiked={ isLiked } clickHandler={ clickLikeHandler }/> }
-          <BkmarkButton isBookmark={ isBookmark } clickHandler={ clickBkmarkHandler }/>
+        </div>
       </div>
+    </div>
 
   );
 }
