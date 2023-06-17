472d417ddfdd41c50cb701509fad3ba3e856a45c
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2023-06-17 17:11:16 +0800

    remove Homepage Footer

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { UserAuth } from "../context/AuthContext";
 import HomePageHeader from "../components/HomePageHeader"
 import ProductGrid from "../components/ProductGrid";
 import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+
 import axios from "axios";
 import './Home.css';
 
@@ -47,8 +47,6 @@ export default function Home() {
     <Navbar/>
     <br />
     <br />
-    <Footer 
-      className = "footer"
-      text="© 2023 OriOri" />
+    
   </>
 )}
