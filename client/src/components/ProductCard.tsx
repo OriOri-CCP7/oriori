e02231df1fcfc72dbc74609a68b196427f5d9102
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2023-06-18 19:01:35 +0800

    feat: created {Fonts,Container,Images}.css

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -6,7 +6,13 @@ import { UserLogs } from '../context/LogContext';
 import BkmarkButton from './BkmarkButton';
 import LikeButton from './LikeButton';
 import LogButton from './LogButton';
+
 import '../styles/Card.css';
+import '../styles/Container.css';
+import '../styles/Fonts.css';
+import '../styles/Icons.css';
+import '../styles/Button.css';
+import '../styles/Images.css';
 
 type  Props = {
   product: Product,
