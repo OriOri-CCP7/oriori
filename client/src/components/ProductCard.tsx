2d3a19f40e4c16e0a8161c959dde0d1a0d9cf71f
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2023-06-19 14:53:05 +0800

    ProductGrid.css is re-stored and appeared ordinary

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -7,12 +7,12 @@ import BkmarkButton from './BkmarkButton';
 import LikeButton from './LikeButton';
 import LogButton from './LogButton';
 
-import '../styles/Card.css';
-import '../styles/Container.css';
-import '../styles/Fonts.css';
-import '../styles/Icons.css';
-import '../styles/Button.css';
-import '../styles/Images.css';
+// import '../styles/Card.css';
+// import '../styles/Container.css';
+// import '../styles/Fonts.css';
+// import '../styles/Icons.css';
+// import '../styles/Button.css';
+// import '../styles/Images.css';
 
 import '../styles/ProductCard.css';
 
