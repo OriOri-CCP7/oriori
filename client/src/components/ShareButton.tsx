3594ac1c166e392d0b98670eba0eed2728991e54
Joe Van Alstyne <6261485+kappanjoe@users.noreply.github.com>
2023-06-21 20:45:17 +0900

    fix: adjust ShareButton structure to match other Product Buttons

--- client/src/components/ShareButton.tsx
+++ client/src/components/ShareButton.tsx
@@ -3,21 +3,19 @@ import { ShareIcon } from '@heroicons/react/24/solid'
 import '../styles/ShareButton.css';
 
 type Props = {
-    hasShared: boolean,
-    clickHandler:React.MouseEventHandler<HTMLDivElement>
+  hasShared: boolean,
+  clickHandler:React.MouseEventHandler<HTMLDivElement>
 };
 
 
 const ShareButton = ( { hasShared, clickHandler }: Props ) => {
 
-    return (
-        <div>
-            <div className='product__button' onClick={clickHandler}>
-                <ShareIcon className="product__icon share__icon solid"/>
-                </div>
-                {hasShared ? "Link Copied" : null }
-        </div>
-    )
-}
+  return (
+    <div className='product__button' onClick={ clickHandler }>
+      <ShareIcon className='product__icon share__icon'/>
+      { hasShared ? 'Link Copied' : null }
+    </div>
+  );
+};
 
 export default ShareButton;
