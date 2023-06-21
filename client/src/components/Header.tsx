808ae4caf77105f5b6f3e863dc2f6ee4c6335527
Joe Van Alstyne <6261485+kappanjoe@users.noreply.github.com>
2023-06-21 22:15:59 +0900

    fix: add alt tag to logo

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -12,8 +12,8 @@ const Header: React.FC<Props> = ({ mainText, secondaryText }: Props) => {
   return (
     <>
     <header
-      className="header" >
-      { mainText ? <h1> { mainText } </h1> : <img src='/oriori-logo.svg' className='header__logo'/> }
+      className="header">
+      { mainText ? <h1> { mainText } </h1> : <img src='/oriori-logo.svg' className='header__logo' alt='OriOri Logo'/> }
       { secondaryText && <h2> { secondaryText } </h2> }
     </header>
     </>
