be0ea139065e72ec476ad5e002ff5f608dd1c3e1
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2023-06-23 18:12:12 +0800

    recreate database

---
+++ loadDummyData.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+filenames=(001_Locations 002_Users 003_Stores 004_Products 005_Bookmarks 006_Logs )
+skipindices=( 0 1 2 4 5 )
+
+for (( i=0; i<${#filenames[@]}; i++ ));
+# python3 manage.py loaddata 00X_Filename.json
+# python3 manage.py loaddata 001_Locations.json 
+# python3 manage.py loaddata 003_Stores.json
+# python3 manage.py loaddata 004_Products.json
+do 
+    if [[ " ${skipindices[@]} " =~ " $i " ]]; 
+    then
+        continue
+    fi
+    python3 manage.py loaddata ${filenames[$i]}.json
+done
