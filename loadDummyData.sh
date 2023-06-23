44dde6286d4ff36a2ca771fc500ad46581717cfe
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2023-06-23 19:09:52 +0800

    testing database

--- loadDummyData.sh
+++ loadDummyData.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 filenames=(001_Locations 002_Users 003_Stores 004_Products 005_Bookmarks 006_Logs )
-skipindices=( 0 1 2 4 5 )
+skipindices=( 0 1 2 3 4 )
 
 for (( i=0; i<${#filenames[@]}; i++ ));
 # python3 manage.py loaddata 00X_Filename.json
@@ -12,5 +12,6 @@ do
     then
         continue
     fi
+    echo "${filenames[$i]}"
     python3 manage.py loaddata ${filenames[$i]}.json
 done
