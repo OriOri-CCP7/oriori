3d0c6b67efaa0f2c5ebe1f646784b6b3dd002e40
ikuno815 <ikuno.kanasugi19@gmail.com>
2023-06-24 09:21:50 +0900

    refactor: remove handleLogout and logout button from HpmePageHeader

--- client/src/components/HomePageHeader.tsx
+++ client/src/components/HomePageHeader.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { UserAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { Cog6ToothIcon } from '@heroicons/react/24/solid';
-//import Button from '../components/Button';
 import prefs from '../data/prefectures.json';
 
 import '../styles/HomePageHeader.css'
@@ -14,26 +13,10 @@ const HomePageHeader = () => {
   const location: number = Number(auth?.user.location);
   const prefectureName: string = prefs[location - 1].name;
   
-  // const handleLogout = async () => {
-  //   try {
-  //     if(auth){
-  //       await auth.logout();
-  //     }
-  //     console.log("User Logged Out");
-  //     navigate('/');
-  //   } catch (error) {
-  //     console.log('🥸', error);
-  //   }
-  // };
 
     return (
     <div className="header home">
       <div className="home-header__top-container">
-        {/* <Button
-          className="logout__button"
-          text="Log Out"
-          type="button"
-          onClick={ handleLogout } /> */}
         <img src='/oriori-logo.svg' className='header__logo' alt='OriOri Logo'/>
         <div className="circular-icon" style={{ background: '#ccc'}}>
           <Cog6ToothIcon 
