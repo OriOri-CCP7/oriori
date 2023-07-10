7b315e2f1f6461efd23e3be840f54c6905f2a644
Joe Van Alstyne <6261485+kappanjoe@users.noreply.github.com>
2023-07-10 17:21:49 +0900

    ci: install lint-staged, run tests on staged files only

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/src/**/__tests__/*.{js,jsx,ts,tsx}', '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}']
 };
