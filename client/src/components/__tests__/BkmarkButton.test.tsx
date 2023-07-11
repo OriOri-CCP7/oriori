bbdd53686d4b64660ee80b3a156d3f2dd7022469
Joe Van Alstyne <6261485+kappanjoe@users.noreply.github.com>
2023-07-11 17:23:56 +0900

    chore: expand BkmarkButton tests

--- client/src/components/__tests__/BkmarkButton.test.tsx
+++ client/src/components/__tests__/BkmarkButton.test.tsx
@@ -6,8 +6,10 @@ test('is solid when bookmarked', async () => {
   // ARRANGE
   render(<BkmarkButton isBookmark={true} clickHandler={() => {}}/>);
   // ACT
+  await screen.findByRole('switch');
   await screen.findByRole('img', {hidden: true});
   // ASSERT
+  expect(screen.getByRole('switch')).toHaveAttribute('aria-checked', 'true');
   expect(screen.getByRole('img', {hidden: true})).toHaveClass('bookmark__icon solid');
 });
 
@@ -15,7 +17,9 @@ test('is outline when not bookmarked', async () => {
   // ARRANGE
   render(<BkmarkButton isBookmark={false} clickHandler={() => {}}/>);
   // ACT
+  await screen.findByRole('switch');
   await screen.findByRole('img', {hidden: true});
   // ASSERT
+  expect(screen.getByRole('switch')).toHaveAttribute('aria-checked', 'false');
   expect(screen.getByRole('img', {hidden: true})).toHaveClass('bookmark__icon outline');
 });
