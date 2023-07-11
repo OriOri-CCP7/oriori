35da9a0d487145e8c92de240584abea39125379e
Joe Van Alstyne <6261485+kappanjoe@users.noreply.github.com>
2023-07-11 13:37:13 +0900

    chore: unit test for BkmarkButton.tsx

--- client/src/components/__tests__/BkmarkButton.test.tsx
+++ client/src/components/__tests__/BkmarkButton.test.tsx
@@ -8,5 +8,14 @@ test('renders as solid when bookmarked', async () => {
   // ACT
   await screen.findByRole('img', {hidden: true});
   // ASSERT
-  expect(screen.getByRole('img', {hidden: true})).toHaveAttribute('class', 'bookmark__icon solid')
+  expect(screen.getByRole('img', {hidden: true})).toHaveAttribute('class', 'bookmark__icon solid');
+});
+
+test('renders as outline when not bookmarked', async () => {
+  // ARRANGE
+  render(<BkmarkButton isBookmark={false} clickHandler={() => {}}/>);
+  // ACT
+  await screen.findByRole('img', {hidden: true});
+  // ASSERT
+  expect(screen.getByRole('img', {hidden: true})).toHaveAttribute('class', 'bookmark__icon outline');
 });
