4b7a8ab895d813fb5b0b8ca01691b56964895985
Joe Van Alstyne <6261485+kappanjoe@users.noreply.github.com>
2023-07-11 14:14:51 +0900

    fix: adjust unit tests to avoid encroaching on integration test level

--- client/src/components/__tests__/BkmarkButton.test.tsx
+++ client/src/components/__tests__/BkmarkButton.test.tsx
@@ -2,20 +2,20 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import BkmarkButton from '../BkmarkButton';
 
-test('renders as solid when bookmarked', async () => {
+test('is solid when bookmarked', async () => {
   // ARRANGE
   render(<BkmarkButton isBookmark={true} clickHandler={() => {}}/>);
   // ACT
   await screen.findByRole('img', {hidden: true});
   // ASSERT
-  expect(screen.getByRole('img', {hidden: true})).toHaveAttribute('class', 'bookmark__icon solid');
+  expect(screen.getByRole('img', {hidden: true})).toHaveClass('bookmark__icon solid');
 });
 
-test('renders as outline when not bookmarked', async () => {
+test('is outline when not bookmarked', async () => {
   // ARRANGE
   render(<BkmarkButton isBookmark={false} clickHandler={() => {}}/>);
   // ACT
   await screen.findByRole('img', {hidden: true});
   // ASSERT
-  expect(screen.getByRole('img', {hidden: true})).toHaveAttribute('class', 'bookmark__icon outline');
+  expect(screen.getByRole('img', {hidden: true})).toHaveClass('bookmark__icon  outline');
 });
