f7073cc809c3a7f623d712e9b4df43442ff3981a
Joe Van Alstyne <6261485+kappanjoe@users.noreply.github.com>
2023-07-11 13:30:59 +0900

    chore: configure testing environment; test BkmarkButton for active state

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,6 +1,13 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['<rootDir>/src/**/__tests__/*.{js,jsx,ts,tsx}', '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}']
+  moduleNameMapper: {
+    '\\.(css|less)$': 'identity-obj-proxy'
+  },
+  testEnvironment: 'jsdom',
+  testMatch: ['<rootDir>/src/**/__tests__/*.{js,jsx,ts,tsx}', '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'],
+  transform: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/fileTransformer.js',
+  }
 };
---
+++ client/src/components/__tests__/BkmarkButton.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import BkmarkButton from '../BkmarkButton';
+
+test('renders as solid when bookmarked', async () => {
+  // ARRANGE
+  render(<BkmarkButton isBookmark={true} clickHandler={() => {}}/>);
+  // ACT
+  await screen.findByRole('img', {hidden: true});
+  // ASSERT
+  expect(screen.getByRole('img', {hidden: true})).toHaveAttribute('class', 'bookmark__icon solid')
+});
