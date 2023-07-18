875fc28fbf918f34b433d5b0765dc586ff5d28ca
nikobatzi1990 <nicole.boci@gmail.com>
2023-07-18 23:39:06 +0900

    wip: all products display on page

--- client/src/pages/AdminDeleteProduct.tsx
+++ client/src/pages/AdminDeleteProduct.tsx
@@ -5,6 +5,8 @@ import axios from 'axios';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../styles/AdminDeleteProduct.css';
+import ProductGrid from '../components/ProductGrid';
+import { ArrowSmallLeftIcon } from '@heroicons/react/24/solid';
 
 function AdminDeleteProduct() {
 
@@ -12,12 +14,24 @@ function AdminDeleteProduct() {
   const auth = UserAuth();
   const [products, setProducts] = useState<Product[]>([]);
 
-  axios.get('')
+  axios.get('api/products/')
+    .then((result) => {
+      console.log(result.data);
+      setProducts(result.data);
+    })
+    .catch((error) => console.log('🥲', error));
+
   return (
     <>
       <Header
         mainText='OriOri Admin'
         secondaryText='Delete Product'/>
+      
+      <div className='admin__back-button'>
+        <ArrowSmallLeftIcon onClick={() => navigate('/home')}/>
+      </div>
+      
+      <ProductGrid productArray={ products } />
 
       <Footer/> 
     </>
