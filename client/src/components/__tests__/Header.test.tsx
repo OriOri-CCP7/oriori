c68bf99b82365750d4f065e7dcc0c725e0cd08dc
nikobatzi1990 <nicole.boci@gmail.com>
2023-07-23 00:16:34 +0900

    chore: write test for mainText

--- client/src/components/__tests__/Header.test.tsx
+++ client/src/components/__tests__/Header.test.tsx
@@ -1,9 +1,13 @@
-import {render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import '@testing-library/jest-dom';
 import Header from '../Header';
 
-test('loads and displays greeting', async () => {
+test('renders mainText prop correctly', () => {
   // Arrange
+  render(<BrowserRouter><Header mainText="Home" /></BrowserRouter>);
   // Act
+  const testText = screen.getByText('Home');
   // Assert
+  expect(testText).toBeInTheDocument();
 });
