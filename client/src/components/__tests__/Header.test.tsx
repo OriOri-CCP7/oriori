cde97e996e325d2e900a2a37594080f9db5d64c9
nikobatzi1990 <nicole.boci@gmail.com>
2023-07-22 18:26:45 +0900

    wip: write tests for Header

---
+++ client/src/components/__tests__/Header.test.tsx
@@ -0,0 +1,9 @@
+import {render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Header from '../Header';
+
+test('loads and displays greeting', async () => {
+  // Arrange
+  // Act
+  // Assert
+});
