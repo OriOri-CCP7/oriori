ed4f52375b1c539cd4aa22198c7e18393ac8024d
nikobatzi1990 <nicole.boci@gmail.com>
2023-07-23 14:20:48 +0900

    chore: check for solid styles

--- client/src/components/__tests__/LikeButton.test.tsx
+++ client/src/components/__tests__/LikeButton.test.tsx
@@ -3,15 +3,18 @@ import '@testing-library/jest-dom';
 import LikeButton from '../LikeButton';
 
 test('is solid when isLiked prop has value true', async () => {
-  //Arrange
-
+  // Arrange
+  render(<LikeButton isLiked={true} clickHandler={() => {}}/>)
   // Act
-
+  await screen.findByRole('switch');
+  await screen.findByRole('img', {hidden: true});
   // Assert
+  expect(screen.getByRole('switch')).toHaveAttribute('aria-checked', 'true');
+  expect(screen.getByRole('img', {hidden: true})).toHaveClass('product__icon like__icon solid');
 });
 
 test('is outline when isLiked prop has value false', async () => {
-  //Arrange
+  // Arrange
 
   // Act
 
@@ -19,7 +22,7 @@ test('is outline when isLiked prop has value false', async () => {
 });
 
 test('displays text "Liked it!" when isLiked is true', async () => {
-  //Arrange
+  // Arrange
 
   // Act
 
@@ -27,7 +30,7 @@ test('displays text "Liked it!" when isLiked is true', async () => {
 });
 
 test('displays text "Liked it?" when isLiked is false', async () => {
-  //Arrange
+  // Arrange
 
   // Act
 
