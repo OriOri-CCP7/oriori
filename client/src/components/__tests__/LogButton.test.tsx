2469bc8ff732f51f6f020bad09337afa348c2cfd
nikobatzi1990 <nicole.boci@gmail.com>
2023-07-23 12:59:15 +0900

    chore: add four tests cases

---
+++ client/src/components/__tests__/LogButton.test.tsx
@@ -0,0 +1,39 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import LogButton from '../LogButton';
+
+test('is solid when isLogged prop has value true', async () => {
+  // ARRANGE
+
+  // ACT
+ 
+  // ASSERT
+  
+});
+
+test('is outline when isLogged prop has value false', async () => {
+  // ARRANGE
+
+  // ACT
+ 
+  // ASSERT
+  
+});
+
+test('displays text "Tried it!" when isLogged is true', async () => {
+  // ARRANGE
+
+  // ACT
+ 
+  // ASSERT
+  
+});
+
+test('displays text "Tried it?" when isLogged is false', async () => {
+  // ARRANGE
+
+  // ACT
+ 
+  // ASSERT
+  
+});
