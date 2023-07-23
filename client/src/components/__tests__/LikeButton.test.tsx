99ccf6ea34a83bf7369c428222c630b31465838d
nikobatzi1990 <nicole.boci@gmail.com>
2023-07-23 14:14:40 +0900

    chore: set up tests for LikeButton

---
+++ client/src/components/__tests__/LikeButton.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import LikeButton from '../LikeButton';
+
+test('is solid when isLiked prop has value true', async () => {
+  //Arrange
+
+  // Act
+
+  // Assert
+});
+
+test('is outline when isLiked prop has value false', async () => {
+  //Arrange
+
+  // Act
+
+  // Assert
+});
+
+test('displays text "Liked it!" when isLiked is true', async () => {
+  //Arrange
+
+  // Act
+
+  // Assert
+});
+
+test('displays text "Liked it?" when isLiked is false', async () => {
+  //Arrange
+
+  // Act
+
+  // Assert
+});
