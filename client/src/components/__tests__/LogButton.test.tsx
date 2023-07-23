ff31bc4c159e969fa5e799e413439ded5a57721e
nikobatzi1990 <nicole.boci@gmail.com>
2023-07-23 13:41:41 +0900

    chore: test for solid styles

--- client/src/components/__tests__/LogButton.test.tsx
+++ client/src/components/__tests__/LogButton.test.tsx
@@ -4,11 +4,13 @@ import LogButton from '../LogButton';
 
 test('is solid when isLogged prop has value true', async () => {
   // ARRANGE
-
+  render(<LogButton isLogged={true} clickHandler={() => {}}/>);
   // ACT
- 
+  await screen.findByRole('switch');
+  await screen.findByRole('img', {hidden: true});
   // ASSERT
-  
+  expect(screen.getByRole('switch')).toHaveAttribute('aria-checked', 'true');
+  expect(screen.getByRole('img', {hidden: true})).toHaveClass('product__icon log__icon solid');
 });
 
 test('is outline when isLogged prop has value false', async () => {
