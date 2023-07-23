7c9d73cdd0c4560efce70e8db1609c424e7c3912
nikobatzi1990 <nicole.boci@gmail.com>
2023-07-23 13:39:15 +0900

    chore: test for outline styles

--- client/src/components/__tests__/LogButton.test.tsx
+++ client/src/components/__tests__/LogButton.test.tsx
@@ -13,11 +13,13 @@ test('is solid when isLogged prop has value true', async () => {
 
 test('is outline when isLogged prop has value false', async () => {
   // ARRANGE
-
+  render(<LogButton isLogged={false} clickHandler={() => {}}/>);
   // ACT
- 
+  await screen.findByRole('switch');
+  await screen.findByRole('img', {hidden: true});
   // ASSERT
-  
+  expect(screen.getByRole('switch')).toHaveAttribute('aria-checked', 'false');
+  expect(screen.getByRole('img', {hidden: true})).toHaveClass('product__icon log__icon outline');
 });
 
 test('displays text "Tried it!" when isLogged is true', async () => {
