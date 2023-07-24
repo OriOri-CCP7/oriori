00ee6ca3651d3674a487ae9397dde34ba95aa10a
nikobatzi1990 <nicole.boci@gmail.com>
2023-07-24 19:20:00 +0900

    chore: test for disabled button

--- client/src/components/__tests__/Button.test.tsx
+++ client/src/components/__tests__/Button.test.tsx
@@ -16,17 +16,17 @@ test('component uses type prop as type', async () => {
   render(<Button className='login-button' text="Submit!" type="submit"/>);
   // Act
   const testButton = await screen.findByRole('button');
-  
   // Assert
   expect(testButton).toHaveAttribute('type', 'submit');
 });
 
-test('component is disabled when disabled is true', () => {
+test('component is disabled when disabled is true', async () => {
   // Arrange
-  
+  render(<Button className='login-button' text="Submit!" type="submit" disabled={true}/>);
   // Act
-  
+  const testButton = await screen.findByRole('button');
   // Assert
+  expect(testButton).toBeDisabled();
 });
 
 test('displays text prop as label on button', () => {
