a06087d3cf89b3b8c07bffe889086593b2e227a4
nikobatzi1990 <nicole.boci@gmail.com>
2023-07-24 18:52:11 +0900

    chore: set up tests for Button

---
+++ client/src/components/__tests__/Button.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Button from '../Button';
+
+test('component uses className prop as class name', () => {
+  // Arrange
+  
+  // Act
+  
+  // Assert
+});
+
+test('component uses type prop as type', () => {
+  // Arrange
+  
+  // Act
+  
+  // Assert
+});
+
+test('component is disabled when disabled is true', () => {
+  // Arrange
+  
+  // Act
+  
+  // Assert
+});
+
+test('displays text prop as label on button', () => {
+  // Arrange
+  
+  // Act
+  
+  // Assert
+});
