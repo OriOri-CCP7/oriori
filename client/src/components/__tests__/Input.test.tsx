3df61d8b691b0f436a030fb07603055a1df96b3e
nikobatzi1990 <nicole.boci@gmail.com>
2023-07-25 20:55:03 +0900

    chore: set up tests for Input

---
+++ client/src/components/__tests__/Input.test.tsx
@@ -0,0 +1,75 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Input from '../Input';
+
+test('component class name matches className prop', () => {
+  // Arrange
+
+  // Act
+  
+  // Assert
+
+});
+
+test('component placeholder text matches placeholder prop', () => {
+  // Arrange
+
+  // Act
+  
+  // Assert
+
+});
+
+test('component has type matching type prop', () => {
+  // Arrange
+
+  // Act
+  
+  // Assert
+
+});
+
+test('component has auto-complete type matching autoComplete prop', () => {
+  // Arrange
+
+  // Act
+  
+  // Assert
+
+});
+
+test('component has value matching value prop', () => {
+  // Arrange
+
+  // Act
+  
+  // Assert
+
+});
+
+test('component value must not be empty when required prop is true', () => {
+  // Arrange
+
+  // Act
+  
+  // Assert
+
+});
+
+test('if type is "file", component only accepts images', () => {
+  // Arrange
+
+  // Act
+  
+  // Assert
+
+});
+
+test('if type is "file", component only accepts one file selection', () => {
+  // Arrange
+
+  // Act
+  
+  // Assert
+
+});
