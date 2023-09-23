a5d306929b5148e5db52c0251c96128ea60f43db
Joe Van Alstyne <6261485+kappanjoe@users.noreply.github.com>
2023-09-23 19:43:31 +0900

    chore: duplicate HomeUser as HomeAnon

---
+++ client/src/pages/HomeAnon.tsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import { UserAuth } from "../context/AuthContext";
+import Header from "../components/Header"
+// TODO: Disable or hide log/like buttons or have them redirect to signup or login
+import ProductGrid from "../components/ProductGrid";
+// TODO: Hide navbar or hide page icons which require login
+import Navbar from "../components/Navbar";
+import prefs from "../data/prefectures.json";
+
+import axios from "axios";
+
+import "../styles/Home.css";
+
+export default function HomeUser() {
+  
+  const auth = UserAuth();
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loadComplete, setLoadComplete] = useState(false);
+  const location: number = Number(auth?.user.location);
+  const prefectureName: string = prefs[location - 1].name;
+  
+  useEffect(() => {
+    let headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'X-CSRFToken': auth?.csrftoken ?? ""
+    };
+    
+    // TODO: Default location? Or show all?
+    axios.get(`/api/locations/${auth?.user.location}/products/`, {
+      headers: headers
+    })
+    .then((response) => {
+      console.log('🏠 HOME: ', response);
+      setProducts(response.data);
+    })
+    .catch((err) => console.log('😈', err))
+    .finally(() => setLoadComplete(true));;
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <Header mainText="Home"/>
+      <div className='page__wrapper'>
+        {
+          products.length > 0
+          ? <>
+                <h2 className="subtitle underlined">
+                  { `Products Available in ${ prefectureName }` }
+                </h2>
+                <ProductGrid productArray={ products }/>
+              </>
+            : loadComplete && <h2 className="subtitle">
+                No products could be found in your Home Prefecture.
+              </h2>
+        }
+      </div>
+      {/* <Navbar/> */}
+    </>
+)}
