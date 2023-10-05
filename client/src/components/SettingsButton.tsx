e59fda7d9f6b6834624e95f7149c8cde9f4680c0
Joe Van Alstyne <6261485+kappanjoe@users.noreply.github.com>
2023-10-05 19:27:38 +0900

    refactor: use wrapper element in SettingsButton

--- client/src/components/SettingsButton.tsx
+++ client/src/components/SettingsButton.tsx
@@ -6,12 +6,14 @@ import '../styles/SettingsButton.css';
 function SettingsButton() {
   const navigate = useNavigate();
   return (
-    <div className="circular-icon">
-      <Cog6ToothIcon 
-        className="settings-icon"
-        onClick = {() => {
-          navigate('/settings');
-        }}/>
+    <div className="circular-icon__wrapper">
+      <div className="circular-icon">
+        <Cog6ToothIcon 
+          className="settings-icon"
+          onClick = {() => {
+            navigate('/settings');
+          }}/>
+      </div>
     </div>
   );
 };
